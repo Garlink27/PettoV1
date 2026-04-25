6dd7309dda9167b88e9a7dd1cb9587def093071c
Panzerfaust13579 <157612913+Panzerfaust13579@users.noreply.github.com>
2026-04-24 19:33:20 -0600

    Revert "Model Upload"
    This reverts commit e6f5522ff9694410d9cf991a141c0f0604fd31d1.

--- PettoV1/SharedResources/Models/CategoriaModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SharedResources.Models
-{
-    public class CategoriaModel
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
-        public ICollection<TareaModel> Tareas { get; set; } = new List<TareaModel>();
-    }
-}
